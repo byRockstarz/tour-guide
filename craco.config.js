9b034ef7ff766b174a24b6bc6c0ae925a64b4cea
byRockstarz <ballzznatthaphon@hotmail.com>
2021-01-22 23:00:51 +0700

    create react app and little structure

---
+++ craco.config.js
@@ -0,0 +1,21 @@
+const CracoLessPlugin = require("craco-less");
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: {
+              "@primary-color": "#4248f5",
+              "@link-color": "#1DA57A",
+              "@border-radius-base": "2px",
+            },
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
+};
